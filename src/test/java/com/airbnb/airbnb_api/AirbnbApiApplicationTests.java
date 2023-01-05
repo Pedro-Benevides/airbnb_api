252a3030181b3782f6130c54076697dcb9dc3f6e
Pedro-Benevides <pvsbenevides197@gmail.com>
2023-01-04 21:15:08 -0300

    first commit

---
+++ src/test/java/com/airbnb/airbnb_api/AirbnbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airbnb.airbnb_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

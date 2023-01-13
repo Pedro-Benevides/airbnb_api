3d3ab03608a68585e448fc25be7052ca499765d0
Pedro-Benevides <pvsbenevides197@gmail.com>
2023-01-12 22:37:08 -0300

    feat(Models): implementa models de cidade, estado e país

---
+++ src/main/java/com/airbnb/airbnb_api/Models/Country.java
@@ -0,0 +1,27 @@
+package com.airbnb.airbnb_api.Models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "country")
+public class Country implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+}
